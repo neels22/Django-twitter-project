5e14952474b203ebbdad3df058feb4293bf11b2f
neels22 <indraneelsarode@gmail.com>
2024-08-14 22:55:48 +0530

    14/08/24 - worked on views - Made a view to display all the tweets - tried to create form for creating a tweet --- not completed -- just added starter code for remaining views

--- twitter_project/tweet/views.py
+++ twitter_project/tweet/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Tweet
+from .forms import TweetForm
+
 
 # Create your views here.
 
@@ -7,3 +10,27 @@ def index(req):
     return render(req,'index.html')
 
 
+def tweet_list(request):
+    tweet = Tweet.objects.all()
+    return render(request, 'tweet_list.html',{
+        "tweets":tweet 
+    })
+
+def tweet_create(request):
+
+    if request.method =="POST":
+        form = TweetForm(request.POST,)
+
+        # ......
+    else:
+        form = TweetForm()
+    return render(request, "tweet_form.html",{
+        "form":form
+    })
+ 
+
+def tweet_edit(request,tweet_id):
+    pass 
+
+def tweet_delete(request,tweet_id):
+    pass 
